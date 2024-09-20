df113029fc3fb4d9d4fdf77fbcf6d017d5b34aa9
C2100178 <taic2100178@student.ctu.edu.vn>
2024-09-20 18:20:33 +0700

    ad bt4

---
+++ for.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Sử dụng vòng lặp for để hiển thị các số từ 1 đến 10
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
